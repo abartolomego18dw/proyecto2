b981649277da23796489b2d0e616b5d69ce9d086
abartolomego18dw <abartolomego18dw@ikzubirimanteo.com>
2019-09-18 13:05:57 +0200

    Prueba

---
+++ Temporada1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/estilo1.css">
+	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+	<title>Temporada 1</title>
+</head>
+<body>
+	<header> <p>STRANGER THINGS</p></header>
+	<nav>
+		<ul>
+			<a href="Temporada1.html"><li>Temporada 1</li></a>
+			<a href="Temporada2.html"><li>Temporada 2</li></a>
+			<a href="Temporada3.html"><li>Temporada 3</li></a>
+			<a href="Reparto.html"><li>Reparto</li></a>
+		</ul>
+	</nav>
+	<section>
+		<div id="trailer1"><iframe width="460" height="215" src="https://www.youtube.com/embed/Wre1F5YyIlA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+		<article id="descripcion1">
+			<p>
+				Desde el primer capítulo, la trama de la primera temporada de Stranger Things se articuló en torno a dos grandes temas: la desaparición de Will (Noah Schnapp) tras ver a una extraña criatura, y la aparición de una misteriosa niña llamada Once (o Eleven, Millie Bobby Brown)
+			</p>
+		</article>
+		<article id="episodios1">
+			<h3>Episodios:</h3>
+			<ol>
+				<li>La desaparición de Will Byers</li>
+				<li>La loca de la calle Maple</li>
+				<li>Todo está bien</li>
+				<li>El cuerpo</li>
+				<li>La pulga y el acróbata</li>
+				<li>El monstruo</li>
+				<li>La bañera</li>
+				<li>El otro lado</li>
+			</ol>
+		</article>
+		
+	</section>
+	<footer>
+		<p>Andoni Bartolomé, Aitor Ortiz y Xabier Jacob <?php echo(strftime("%d/%m/%Y %H:%M")); ?>
+		<a href="https://twitter.com/Stranger_Things?ref_src=twsrc%5Etfw"class="twitter-follow-button"data-show-count="false">Follow</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+		</p>
+	</footer>
+
+</body>
+</html>
